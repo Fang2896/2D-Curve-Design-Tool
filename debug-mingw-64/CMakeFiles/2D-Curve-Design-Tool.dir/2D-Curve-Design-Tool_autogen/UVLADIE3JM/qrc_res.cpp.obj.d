0ffedc51febfe19d4e89abf942c73455055a62d5
M1kanN <fangliu2896@gmail.com>
2023-08-31 22:31:24 +0900

    modified gitignore

--- debug-mingw-64/CMakeFiles/2D-Curve-Design-Tool.dir/2D-Curve-Design-Tool_autogen/UVLADIE3JM/qrc_res.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/2D-Curve-Design-Tool.dir/2D-Curve-Design-Tool_autogen/UVLADIE3JM/qrc_res.cpp.obj: \
- E:\ToyPrograms\GL\2D-Curve-Design-Tool\debug-mingw-64\2D-Curve-Design-Tool_autogen\UVLADIE3JM\qrc_res.cpp
